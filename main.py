29dfa5de57d935b09a7f82c8212ffbb2a4024991
MikesoWeb <klionpsyx@gmail.com>
2020-01-21 06:25:43 +0000

    some changes

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 f = open('pars.txt')
-x = f.read(-100)
+x = f.read()
 x = x.replace('0','').replace('1','').replace('2','').replace('3','').replace('4','').replace('5','').replace('6','').replace('7','').replace('8','').replace('9','').replace('0','').replace(':','')
+print(x)
 d = open('res.txt', 'w')
 d.writelines(x)
 d.close()
